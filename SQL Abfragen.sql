43c26df15bde9ee0de82d57d8af99859a3d599a8
FelixB1234 <baumgartner850@gmail.com>
2024-11-08 10:29:04 +0100

    Update SQL Abfragen.sql

--- SQL Abfragen.sql
+++ SQL Abfragen.sql
@@ -1,49 +1,48 @@
 -- Abfrage der relevanten Daten von DimCustomer
 SELECT 
-  c.customerkey AS 'Kundenschlüssel',
+  c.customerkey AS 'KundenschlÃ¼ssel',
   c.firstname AS Vorname,
   c.lastname AS Nachname, 
   c.firstname + ' ' + lastname AS Vollname,   -- Vor- und Nachname kombiniert
   CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Geschlecht,
   c.datefirstpurchase AS DatumErsterKauf,
   g.city AS Stadt                             -- Joined in Stadt (des Kunden) von Geography Tabelle
-FROM 
-  DimCustomer as c
+FROM DimCustomer as c 
   LEFT JOIN dimgeography AS g ON g.geographykey = c.geographykey 
 ORDER BY 
-  CustomerKey ASC -- Liste nach Kundenschlüssel geordnet
+CustomerKey ASC -- Liste nach KundenschlÃ¼ssel geordnet
 
 
 -- Abfrage der relevanten Daten von DimDate 
-SELECT [DateKey] AS Datumschlüssel,
-       [FullDateAlternateKey] AS Datum,
- [EnglishDayNameOfWeek] AS Tag,
- [WeekNumberOfYear] AS WochenNr,
- [EnglishMonthName] AS Monat,
- LEFT([EnglishMonthName], 3) AS Monatkurz, 
- [MonthNumberOfYear] AS MonatNr,
- [CalendarQuarter] AS Quartal,
- [CalendarYear] AS Jahr
- 
+SELECT 
+  DateKey AS DatumschlÃ¼ssel,
+  FullDateAlternateKey AS Datum,
+  EnglishDayNameOfWeek AS Tag,
+  WeekNumberOfYear AS WochenNr,
+  EnglishMonthName AS Monat,
+  LEFT(EnglishMonthName, 3) AS Monatkurz, 
+  MonthNumberOfYear AS MonatNr,
+  CalendarQuarter AS Quartal,
+  CalendarYear AS Jahr
 FROM [DimDate]
 WHERE CalendarYear >= 2022
 
 
 -- Abfrage der relevanten Daten von DimProduct 
 SELECT 
-  p.[ProductKey] AS Produktschlüssel, 
-  p.[ProductAlternateKey] AS Produktschlüssel2, 
-  p.[EnglishProductName] AS Produktname, 
+  p.ProductKey AS ProduktschlÃ¼ssel, 
+  p.ProductAlternateKey AS ProduktschlÃ¼ssel2, 
+  p.EnglishProductName AS Produktname, 
   ps.EnglishProductSubcategoryName AS Subkategorie, -- Joined in von Sub Category Tabelle
   pc.EnglishProductCategoryName AS Produktkategorie, -- Joined in von Category Tabelle
-  p.[Color] AS Produktfarbe, 
-  p.[Size] AS Produktgröße, 
-  p.[ProductLine] AS Produktlinie, 
-  p.[ModelName] AS Produktmodellname, 
-  p.[EnglishDescription] AS Produktbeschreibung, 
+  p.Color AS Produktfarbe, 
+  p.Size AS ProduktgrÃ¶ÃŸe, 
+  p.ProductLine AS Produktlinie, 
+  p.ModelName AS Produktmodellname, 
+  p.EnglishDescription AS Produktbeschreibung, 
   ISNULL (p.Status, 'Outdated') AS Produktstatus 
 FROM 
-  [DimProduct] as p
+  DimProduct as p
   LEFT JOIN DimProductSubcategory AS ps ON ps.ProductSubcategoryKey = p.ProductSubcategoryKey 
   LEFT JOIN DimProductCategory AS pc ON ps.ProductCategoryKey = pc.ProductCategoryKey 
 ORDER BY 
@@ -52,14 +51,14 @@ ORDER BY
 
 -- Abfrage der relevanten Daten von FactInternetSales
 SELECT 
-      [ProductKey] AS Produktschlüssel,
-      [OrderDateKey] AS Bestelldatumschlüssel,
-      [DueDateKey] AS Fälligkeitsdatumschlüssel,
-      [ShipDateKey] AS Versanddatumschlüssel,
-      [CustomerKey]AS Kundenschlüssel,
-      [SalesOrderNumber]AS Bestellnummer,
-      [SalesAmount] AS Umsatz
-FROM  [FactInternetSales]
+      ProductKey AS ProduktschlÃ¼ssel,
+      OrderDateKey AS BestelldatumschlÃ¼ssel,
+      DueDateKey AS FÃ¤lligkeitsdatumschlÃ¼ssel,
+      ShipDateKey AS VersanddatumschlÃ¼ssel,
+      CustomerKey AS KundenschlÃ¼ssel,
+      SalesOrderNumber AS Bestellnummer,
+      SalesAmount AS Umsatz
+FROM  FactInternetSales
 WHERE 
  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- Immer nur 2 Jahre seit Kauf
 ORDER BY 
